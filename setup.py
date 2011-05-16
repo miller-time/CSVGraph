cf20a18b0260b348fabc9e2c66228897a4533fb6
Russell <miller.time.baby@gmail.com>
2011-05-16 08:48:50 -0700

    Hmm, just realized I forgot 'author' and felt like updating the description

--- setup.py
+++ setup.py
@@ -3,7 +3,9 @@ from setuptools import setup,find_packages
 setup(
     name="csvgraph",
     version="0.8",
-    description="Plot pairs of columns from a CSV file to make a PNG.",
+    description=("Use Matplotlib to plot lines from a CSV file and create a"
+                 "PNG image file from the figure."),
+    author="Russell Miller",
     install_requires=['argparse', 'matplotlib'],
     packages=find_packages(),
     entry_points=
