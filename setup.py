eaa0957543f23f953e0d86ea40190d4c905aa7aa
Russell <miller.time.baby@gmail.com>
2011-05-16 08:45:38 -0700

    cleaned up the demo files, setup file was missing a comma

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name="csvgraph",
     version="0.8",
     description="Plot pairs of columns from a CSV file to make a PNG.",
-    install_requires=['argparse', 'matplotlib']
+    install_requires=['argparse', 'matplotlib'],
     packages=find_packages(),
     entry_points=
         """
